691a266dab5d74867632fa22e3aae6146fa8720a
EricdSouzx <eericdsn@gmail.com>
2025-09-16 12:06:17 -0300

    chore: commit initial

---
+++ src/test/java/com/example/bankopsapi/BankopsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankopsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankopsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
