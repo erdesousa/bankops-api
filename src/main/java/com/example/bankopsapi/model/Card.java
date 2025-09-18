1bf7668d2a7769a0be103a8a4150f243612cfa0c
EricdSouzx <eericdsn@gmail.com>
2025-09-17 22:38:58 -0300

    feat(model): criar IssuerEntity com JPA, relacionamentos e Lombok

---
+++ src/main/java/com/example/bankopsapi/model/Card.java
@@ -0,0 +1,4 @@
+package com.example.bankopsapi.model;
+
+public class Card {
+}
---
+++ src/main/java/com/example/bankopsapi/model/Issuer.java
@@ -0,0 +1,29 @@
+package com.example.bankopsapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "issuers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Issuer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_issuer;
+
+    @Column(nullable = false)
+    private String name;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToOne(mappedBy = "issuer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Card card;
+}
