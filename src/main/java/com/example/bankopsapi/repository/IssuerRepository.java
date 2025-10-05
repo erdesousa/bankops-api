9acf3f9efae271b05356140da764ed1f33820005
EricdSouzx <eericdsn@gmail.com>
2025-10-05 19:20:56 -0300

    feat(repository): criar IssuerRepository com JPA

---
+++ src/main/java/com/example/bankopsapi/repository/IssuerRepository.java
@@ -0,0 +1,8 @@
+package com.example.bankopsapi.repository;
+
+import com.example.bankopsapi.model.Issuer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IssuerRepository extends JpaRepository<Issuer, Long> { }
